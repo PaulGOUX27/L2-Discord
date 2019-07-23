efd0bde31a308108bdd36b3f4367e365922d53f0
Paul Goux <paul.goux@wanadoo.fr>
2019-07-23 22:15:00 +0200

    Standing not found, nik

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 const tokenFile  = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const request = require('request');
+const frId = 46;
+const l2Id = 177;
 
 //develop
 
@@ -10,8 +13,12 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if (msg.content === 'ping') {
-        msg.reply('Pong!');
+        request('https://apiv2.apifootball.com/?action=get_standings&league_id=' + l2Id + '&APIkey=' + tokenFile.apiKey, {json: true}, (err, res, body) => {
+            console.log(body);
+
+        })
     }
 });
+
 
 client.login(tokenFile.token);
