0c96734d96b04b0f42c2891eb8091120a5796d99
Paul Goux <paul.goux@wanadoo.fr>
2019-07-30 00:32:31 +0200

    1er classement

--- src/main.js
+++ src/main.js
@@ -4,21 +4,41 @@ const client = new Discord.Client();
 const request = require('request');
 const frId = 46;
 const l2Id = 177;
+const smc = "<:smc:382991999495831566>";
+const lemans = "<:lemans:588053542451806208>";
 
+//client.emojis est un enum int(id) -> emoji(object)
+//const smc = client.emojis.find(emoji => emoji.name === "smc");
+//const lemans = client.emojis.find(emoji => emoji.name === "lemans");
 //develop
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+/*client.on('message', () => {
+    console.log(client.emojis) // Return empty collection
+});*/
+
 client.on('message', msg => {
     if (msg.content === 'ping') {
         request('https://apiv2.apifootball.com/?action=get_standings&league_id=' + l2Id + '&APIkey=' + tokenFile.apiKey, {json: true}, (err, res, body) => {
-            console.log(body);
-
+            //console.log(body);
+            let classementLigue2 = "__Classement Ligue 2__\n";
+            body.forEach(function (club) {
+                classementLigue2 += club["overall_league_position"] + ": ";
+                let clubName = club["team_name"];
+                if(clubName === "Caen")
+                    classementLigue2 += `${smc}`;
+                else if (clubName === "Le Mans")
+                    classementLigue2 += `${lemans}`;
+                else
+                    classementLigue2 += club["team_name"];
+                classementLigue2 += "\n";
+            });
+            msg.channel.send(classementLigue2);
         })
     }
 });
-
 
 client.login(tokenFile.token);
