80ee831965dcfbb2c3fb4cecb7175b83580ebdfe
Paul Goux <paul.goux@wanadoo.fr>
2019-11-10 15:34:17 +0100

    fix spaces

--- src/main.js
+++ src/main.js
@@ -1,9 +1,33 @@
-const tokenFile  = require('./config.json');
+const tokenFile = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const request = require('request');
 const frId = 46;
 const l2Id = 177;
+
+const spaceArray = {
+    "Lorient": "                      ",
+    "AC Ajaccio": "              ",
+    "Troyes": "                      ",
+    "Sochaux": "                   ",
+    "Lens": "                          ",
+    "Clermont": "                 ",
+    "Guingamp": "               ",
+    "Grenoble": "                 ",
+    "Le Havre": "                  ",
+    "Nancy": "                      ",
+    "Rodez": "                       ",
+    "Auxerre": "                   ",
+    "Valenciennes": "         ",
+    "Caen": "                           ",
+    "Niort": "                        ",
+    "Chateauroux": "          ",
+    "Le Mans": "                            ",
+    "Chambly": "                 ",
+    "Paris FC": "                   ",
+    "Orleans": "                   ",
+};
+
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -15,16 +39,17 @@ client.on('message', msg => {
             //console.log(body);
             let classementLigue2 = "__Classement Ligue 2__\n";
             body.forEach(function (club) {
-                classementLigue2 += club["overall_league_position"] + ": ";
+                const position = club["overall_league_position"];
+                classementLigue2 += ((position < 10) ? position + " " : position) + ": ";
                 let clubName = club["team_name"];
-                if(clubName === "Caen")
+                if (clubName === "Caen")
                     classementLigue2 += client.emojis.find(emoji => emoji.name === "smc");
                 else if (clubName === "Le Mans")
                     classementLigue2 += client.emojis.find(emoji => emoji.name === "lemans");
                 else
                     classementLigue2 += club["team_name"];
 
-                classementLigue2 += "             ";
+                classementLigue2 += spaceArray[club["team_name"]];
                 classementLigue2 += club["overall_league_PTS"] + "pts";
                 classementLigue2 += "\n";
             });
