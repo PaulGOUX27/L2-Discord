8c7e45775388bd15b33e4f39113abd86a2de75af
Paul Goux <paul.goux@wanadoo.fr>
2019-11-10 15:13:06 +0100

    auto find emoji

--- src/main.js
+++ src/main.js
@@ -4,21 +4,10 @@ const client = new Discord.Client();
 const request = require('request');
 const frId = 46;
 const l2Id = 177;
-const smc = "<:smc:382991999495831566>";
-const lemans = "<:lemans:588053542451806208>";
-
-//client.emojis est un enum int(id) -> emoji(object)
-//const smc = client.emojis.find(emoji => emoji.name === "smc");
-//const lemans = client.emojis.find(emoji => emoji.name === "lemans");
-//develop
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-
-/*client.on('message', () => {
-    console.log(client.emojis) // Return empty collection
-});*/
 
 client.on('message', msg => {
     if (msg.content === 'ping') {
@@ -29,9 +18,9 @@ client.on('message', msg => {
                 classementLigue2 += club["overall_league_position"] + ": ";
                 let clubName = club["team_name"];
                 if(clubName === "Caen")
-                    classementLigue2 += `${smc}`;
+                    classementLigue2 += client.emojis.find(emoji => emoji.name === "smc");
                 else if (clubName === "Le Mans")
-                    classementLigue2 += `${lemans}`;
+                    classementLigue2 += client.emojis.find(emoji => emoji.name === "lemans");
                 else
                     classementLigue2 += club["team_name"];
                 classementLigue2 += "\n";
