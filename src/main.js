99aa528ff90cbdc9226da34c5c697dbc8e5f356c
PaulGOUX27 <paul.goux@wanadoo.fr>
2020-08-08 18:48:26 +0200

    Update dep & add new l2 clubs

--- src/main.js
+++ src/main.js
@@ -4,28 +4,33 @@ const client = new Discord.Client();
 const request = require('request');
 const frId = 46;
 const l2Id = 177;
+const constSpace = "         ";
 
 const spaceArray = {
-    "Lorient": "                      ",
-    "AC Ajaccio": "              ",
-    "Troyes": "                      ",
-    "Sochaux": "                   ",
-    "Lens": "                          ",
-    "Clermont": "                 ",
-    "Guingamp": "               ",
-    "Grenoble": "                 ",
-    "Le Havre": "                  ",
-    "Nancy": "                      ",
-    "Rodez": "                       ",
-    "Auxerre": "                   ",
-    "Valenciennes": "         ",
-    "Caen": "                           ",
-    "Niort": "                        ",
-    "Chateauroux": "          ",
-    "Le Mans": "                            ",
-    "Chambly": "                 ",
-    "Paris FC": "                   ",
-    "Orleans": "                   ",
+    "Lorient": "             ",
+    "AC Ajaccio": "     ",
+    "Troyes": "             ",
+    "Sochaux": "          ",
+    "Lens": "                 ",
+    "Clermont": "       ",
+    "Guingamp": "       ",
+    "Grenoble": "         ",
+    "Le Havre": "          ",
+    "Nancy": "              ",
+    "Rodez": "              ",
+    "Auxerre": "             ",
+    "Valenciennes": " ",
+    "Caen": "                    ",
+    "Niort": "                ",
+    "Chateauroux": "  ",
+    "Le Mans": "                   ",
+    "Chambly": "        ",
+    "Paris FC": "          ",
+    "Orleans": "          ",
+    "Amiens": "            ",
+    "Pau FC": "             ",
+    "Toulouse": "         ",
+    "Dunkerque": "     ",
 };
 
 
@@ -37,7 +42,7 @@ client.on('message', msg => {
     if (msg.content === 'l2') {
         request('https://apiv2.apifootball.com/?action=get_standings&league_id=' + l2Id + '&APIkey=' + tokenFile.apiKey, {json: true}, (err, res, body) => {
             //console.log(body);
-            let classementLigue2 = "__Classement Ligue 2__\n";
+            let classementLigue2 = "\n__Classement Ligue 2__\n";
             body.forEach(function (club) {
                 const position = club["overall_league_position"];
                 classementLigue2 += ((position < 10) ? position + " " : position) + ": ";
@@ -49,7 +54,7 @@ client.on('message', msg => {
                 else
                     classementLigue2 += club["team_name"];
 
-                classementLigue2 += spaceArray[club["team_name"]];
+                classementLigue2 += spaceArray[club["team_name"]] + constSpace;
                 classementLigue2 += club["overall_league_PTS"] + "pts";
                 classementLigue2 += "\n";
             });
